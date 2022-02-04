24755376eb96860f02d20d7765f6f459b64b2b10
Ishaan-11 <ishaansharma1998@gmail.com>
2022-02-04 21:27:24 +0530

    add: drag drop functionality

--- index.js
+++ index.js
@@ -11,12 +11,8 @@ const COLORS = [
 ];
 
 const parentEle = document.getElementById("parent");
-
-function setAttributes(element, attributes) {
-  Object.keys(attributes).forEach((attr) => {
-    element.setAttribute(attr, attributes[attr]);
-  });
-}
+let dragindex = 0;
+let clone = "";
 
 // set colors of boxes
 for (let i = 0; i < COLORS.length; i++) {
@@ -24,6 +20,10 @@ for (let i = 0; i < COLORS.length; i++) {
   const attributes = {
     id: `box${i}`,
     class: "empty",
+    draggable: "true",
+    ondragstart: "drag(event)",
+    ondrop: "drop(event)",
+    ondragover: "allowDrop(event)",
     style: `background-color: ${COLORS[i]};`,
   };
 
@@ -32,3 +32,41 @@ for (let i = 0; i < COLORS.length; i++) {
   parentEle.append(div);
 }
 
+function setAttributes(element, attributes) {
+  Object.keys(attributes).forEach((attr) => {
+    element.setAttribute(attr, attributes[attr]);
+  });
+}
+
+function drag(e) {
+  e.dataTransfer.setData("text", e.target.id);
+}
+
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+function drop(e) {
+  e.preventDefault();
+  clone = e.target.cloneNode(true);
+  let data = e.dataTransfer.getData("text");
+  if (clone.id !== data) {
+    let nodelist = document.getElementById("parent").childNodes;
+    for (let i = 0; i < nodelist.length; i++) {
+      if (nodelist[i].id == data) {
+        dragindex = i;
+      }
+    }
+
+    document
+      .getElementById("parent")
+      .replaceChild(document.getElementById(data), e.target);
+
+    document
+      .getElementById("parent")
+      .insertBefore(
+        clone,
+        document.getElementById("parent").childNodes[dragindex]
+      );
+  }
+}
